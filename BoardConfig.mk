00389ecf5fed74f1446da590a1d7fa2d73289236
Sandeep-FED <sandymankara11@gmail.com>
2022-10-30 08:22:21 +0000

    TMP: Dont use nxp for now
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/cherish/config/device_framework_matrix.xml \
-    vendor/nxp/nfc/vendor_framework_compatibility_matrix.xml
+    #vendor/nxp/nfc/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_yupik.xml
 
